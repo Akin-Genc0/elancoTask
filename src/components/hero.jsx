4a007111c843f660beb5e38629b250b2027bca77
Akin-Genc0 <akingenc212@gmail.com>
2024-11-20 14:43:39 +0000

    Added responsive and visually appealing hero section to Home page.

---
+++ src/components/hero.jsx
@@ -0,0 +1,28 @@
+import './hero.css';
+import bgVideo from '../assets/bgVideo.mp4'; 
+
+function Hero() {
+
+    return (
+        <>
+        
+        <div className='hero-con'>
+         <p></p>
+        <video autoPlay muted loop className="bg-video">
+                <source src={bgVideo} type="video/mp4" />
+              
+            </video>
+            <div className="content">
+                <h1>Switzerland</h1>
+                <p className='sub-text'>Explore with just a tap</p>
+                <button>Explore Countries</button>
+            </div>
+            </div>
+            
+       
+ 
+        </>
+    );
+}
+
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,14 @@
 import Nav from '../components/nav';
- 
+import Hero from '../components/hero';
 
 function Home() {
   return (
     <div>
        <Nav/>
-        
+        <Hero/>
     </div>
    
   );
 }
 
 export default Home;
-
