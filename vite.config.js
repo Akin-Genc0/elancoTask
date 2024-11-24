2784fb50921c3c721b52a4393ba1cee8a386e0bb
Akin-Genc0 <akingenc212@gmail.com>
2024-11-24 14:16:28 +0000

    change incorrect name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/elancoTask/', // This ensures the app is served correctly from the repository's subpath
+  base: "/elancoTask", 
 });
